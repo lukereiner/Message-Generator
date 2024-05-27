f412e44000cae2a25268ccc3c531c1259232764d
lukereiner <72576375+lukereiner@users.noreply.github.com>
2024-05-27 10:28:04 -0400

    add NBA data of scoring, MVPs, and NBA champions

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log('Hello world');
+const nbaData = {
+    scoring: {
+        2024: 'Luka Doncic',
+        2023: 'Joel Embiid',
+        2022: 'Joel Embiid',
+        2021: 'Stephen Curry',
+        2020: 'James Harden',
+        2019: 'James Harden'
+    },
+    mvp: {
+        2024: 'Nikola Jokic',
+        2023: 'Joel Embiid',
+        2022: 'Nikola Jokic',
+        2021: 'Nikola Jokic',
+        2020: 'Giannis Antetokounmpo',
+        2019: 'Giannis Antetokounmpo'
+    },
+    titles: {
+        2023: 'Denver Nuggets',
+        2022: 'Golden State Warriors',
+        2021: 'Milwaukee Bucks',
+        2020: 'Los Angeles Lakers',
+        2019: 'Toronto Raptors',
+        2018: 'Golden State Warriors'
+    }
+}
+
+console.log(nbaData['mvp'][2024]);
