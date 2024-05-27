ddea1e716e6c7a4fd83335d9464d7698ff6bdab7
lukereiner <72576375+lukereiner@users.noreply.github.com>
2024-05-27 11:50:16 -0400

    checkpoint - need to functionality for random key selection

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const nbaData = {
     scoring: {
-        2024: 'Luka Doncic',
         2023: 'Joel Embiid',
         2022: 'Joel Embiid',
         2021: 'Stephen Curry',
@@ -8,7 +7,6 @@ const nbaData = {
         2019: 'James Harden'
     },
     mvp: {
-        2024: 'Nikola Jokic',
         2023: 'Joel Embiid',
         2022: 'Nikola Jokic',
         2021: 'Nikola Jokic',
@@ -21,8 +19,35 @@ const nbaData = {
         2021: 'Milwaukee Bucks',
         2020: 'Los Angeles Lakers',
         2019: 'Toronto Raptors',
-        2018: 'Golden State Warriors'
     }
 }
 
-console.log(nbaData['mvp'][2024]);
+let genResponse = [];
+
+function randNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function messageOutput() {
+    for (type in nbaData) {
+        let listID = randNum(2019, 2023);
+
+        switch (type) {
+            case 'scoring':
+                genResponse.push(`The scoring champion for ${listID} was: ${nbaData[type][listID]}`);
+                break;
+            case 'mvp':
+                genResponse.push(`The league MVP for ${listID} was: ${nbaData[type][listID]}`);
+                break;
+            case 'titles':
+                genResponse.push(`The NBA champion for ${listID} was: ${nbaData[type][listID]}`);
+                break;
+            default:
+                return 'error';
+        }
+
+    };
+    return genResponse;
+};
+
+console.log(messageOutput());
